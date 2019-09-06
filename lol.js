2b2d0c3e9bcffb041042ad03889ef90301c8b8e3
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-06 12:26:21 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -4,10 +4,14 @@ window.onunload = function(){
     theDoc = theWindow.document,
     theScript = document.createElement('script');
   function injectThis() {
-    alert(document.body.innerHTML);
-    var ele = document.createElement('script');
-    ele.src = "https://jeffnjellybean.github.io/schoolhax/lol.js";
-    document.body.appendChild(ele);
+    try {
+      alert(document.body.innerHTML);
+      var ele = document.createElement('script');
+      ele.src = "https://jeffnjellybean.github.io/schoolhax/lol.js";
+      document.body.appendChild(ele);
+    } catch (e) {
+      alert(e.message);
+    }
   }
   theScript.innerHTML = 'window.onload = ' + injectThis.toString() + ';';
   theDoc.body.appendChild(theScript);
