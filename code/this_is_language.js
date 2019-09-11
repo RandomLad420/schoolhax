889d6c2df14093974b14cc6b59dca4458b05230e
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-11 18:44:09 -0400

    Update this_is_language.js

--- code/this_is_language.js
+++ code/this_is_language.js
@@ -1,1 +1,98 @@
-try{alert("\nThank you for using JeffNJellyBean's bot!\nHow it works:\nEvery time the bot comes across a term it doesn't know, it will ask you.\nMAKE SURE YOU ANSWER CORRECTLY, as it will then use your answer every time that same term comes up again.\n       ");var match={};function getAnswer(){for(var e=setTimeout(";"),t=0;t<e;t++)clearTimeout(t);var r=document.querySelector(".gender").innerText,n=document.querySelector("input"),a=document.querySelector(".next_btn"),o=match[r];if("string"!=typeof o){var i=prompt(r);if(null==i)return void alert("Aborting...");match[r]=i,o=match[r]}n.setAttribute("value",o),a.click(),a.click(),setTimeout(getAnswer,500)}getAnswer()}catch(e){alert(`\nIt appears there was an error.\nTechnical stuff:\n${e.message}\n       `)}
+function getAnswer() {
+  if(worker == undefined) {
+    worker = new Worker('ocrad_worker.js');
+    worker.onmessage = function(e) {
+      var result = e.data;
+    }
+  }
+  worker.postMessage($('#word_canvas')[0]);
+  console.log(result);
+
+  var text = result;
+  var answer = translations1[text];
+
+  if (answer == undefined) {
+    answer = translations2[text];
+  }
+
+  console.log(translations1);
+  console.log(translations2);
+  console.log(answer);
+
+  $('.guess').val(answer);
+
+  var e = jQuery.Event("keypress");
+  e.which = 13;
+  $('.guess').trigger(e);
+
+  getAnswer();
+}
+
+function answerQuestion() {
+  var type = $('.gender').text();
+  var answer = translations[type];
+
+  $('input').val(answer);
+
+  var e = jQuery.Event("keydown");
+  e.which = 13;
+  $('input').trigger(e);
+
+  setTimeout(answerQuestion, 250);
+}
+try {
+  if (window.location.href.indexOf('game/user_list') !== -1) {
+
+    var ele = document.createElement('script');
+    ele.src = 'https://jeffnjellybean.github.io/schoolhax/code/ocrad.js';
+    document.body.appendChild(ele);
+
+    ele = document.createElement('script');
+    ele.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    document.body.appendChild(ele);
+
+    var translations1 = {};
+    var translations2 = {};
+    var tables = document.querySelectorAll('tr');
+
+    for (i = 0; i < tables.length; i++) {
+      var childs = tables[i].children;
+      translations1[childs[0].innerText] = childs[1].innerText;
+      translations2[childs[1].innerText] = childs[0].innerText;
+    }
+
+    if (confirm('OK to play, cancel to Level Up')) {
+      $('.play_btn').click()
+    } else {
+      $('.level_up_btn').click();
+    }
+
+    var worker;
+    
+    var interval = setInterval(function() {
+      if ($('.delay').text() === '1') {
+        clearTimeout(interval);
+        setTimeout(getAnswer, 1000);
+      }
+    }, 500);
+
+  } else if (window.location.href.indexOf('/grammar') !== -1) {
+
+    ele = document.createElement('script');
+    ele.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    document.body.appendChild(ele);
+    var translations = {};
+
+    $('tr').each(function(i) {
+      var childs = $(this).children();
+      var type = childs.first().text().replace('masculine', 'm').replace('feminine', 'f').replace('singular', '(s)').replace('plural', '(pl)').trim();
+      translations[type] = childs.eq(1).text().trim();
+    });
+
+    $('.start_testing_btn').click();
+
+    answerQuestion();
+  }
+} catch (e) {
+  alert(e.message);
+}
