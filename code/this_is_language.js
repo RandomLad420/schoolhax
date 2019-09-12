b2360e8b36827a60bef13a1b3a8341249f365ec8
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-12 09:38:56 -0400

    Update this_is_language.js

--- code/this_is_language.js
+++ code/this_is_language.js
@@ -1,7 +1,7 @@
 function getAnswer() {
   try {
-  if (lastWorker) lastWorker.terminate();
-  var blob = new Blob([`
+    if (lastWorker) lastWorker.terminate();
+    var blob = new Blob([`
 importScripts('https://jeffnjellybean.github.io/schoolhax/code/gocr.js')
 onmessage = function(e){
 var result = GOCR(e.data);
@@ -9,35 +9,35 @@ postMessage(result);
 }
     `]);
 
-  var blobURL = window.URL.createObjectURL(blob);
+    var blobURL = window.URL.createObjectURL(blob);
 
-  worker = new Worker(blobURL);
+    worker = new Worker(blobURL);
 
-  worker.onmessage = function(e) {
-    var result = e.data;
-    alert(result);
+    worker.onmessage = function(e) {
+      var result = e.data;
+      alert(result);
 
-    var text = result;
-    var answer = translations1[text];
+      var text = result;
+      var answer = translations1[text];
 
-    if (answer == undefined) {
-      answer = translations2[text];
+      if (answer == undefined) {
+        answer = translations2[text];
+      }
+
+      alert(answer);
+
+      $('.guess').val(answer);
+
+      var event = jQuery.Event('keypress');
+      event.which = 13;
+      $('.guess').trigger(event);
+
+      worker.terminate();
+      setTimeout(getAnswer, 1000);
     }
 
-    alert(answer);
-
-    $('.guess').val(answer);
-
-    var e = jQuery.Event("keypress");
-    e.which = 13;
-    $('.guess').trigger(e);
-
-    worker.terminate();
-    setTimeout(getAnswer, 1000);
-  }
-
-  worker.postMessage($('#word_canvas')[0].toDataURL("image/png"));
-  lastWorker = worker;
+    worker.postMessage($('#word_canvas')[0].toDataURL('image/png'));
+    lastWorker = worker;
   } catch (e) {
     alert(e.message);
   }
@@ -58,7 +58,7 @@ function answerQuestion() {
 try {
   if (window.location.href.indexOf('game/user_list') !== -1) {
 
-    ele = document.createElement('script');
+    var ele = document.createElement('script');
     ele.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
     document.body.appendChild(ele);
 
@@ -66,7 +66,7 @@ try {
     var translations2 = {};
     var tables = document.querySelectorAll('tr');
 
-    for (i = 0; i < tables.length; i++) {
+    for (var i = 0; i < tables.length; i++) {
       var childs = tables[i].children;
       translations1[childs[0].innerText] = childs[1].innerText;
       translations2[childs[1].innerText] = childs[0].innerText;
@@ -89,7 +89,7 @@ try {
 
   } else if (window.location.href.indexOf('/grammar') !== -1) {
 
-    ele = document.createElement('script');
+    var ele = document.createElement('script');
     ele.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
     document.body.appendChild(ele);
     var translations = {};
